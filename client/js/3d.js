41d31cb011a63b82e704aeb087a352fbd80f57d6
owen-d <ow.diehl@gmail.com>
2014-12-04 16:14:07 -0800

    display working, going to attempt to re-draw with new constructor functions ripped from chromeVRExperiments

--- client/js/3d.js
+++ client/js/3d.js
@@ -3,11 +3,11 @@ var filename = 'photos/dolphin.jpg';
 
 function imageInit(url, filename) {
   var pic = THREE.ImageUtils.loadTexture(
-      url + filename
+      '../client/photos/dolphin.jpg'
   );
   var planegeometry = new THREE.PlaneGeometry( 635, 441 );
   var planematerial = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
+    color: 0x99CCFF,
     // side: THREE.DoubleSide,
     map: pic
   });
@@ -15,4 +15,7 @@ function imageInit(url, filename) {
   scene.add( plane );
 
   camera.position.y = 441 / 1.5;
+
 }
+
+
